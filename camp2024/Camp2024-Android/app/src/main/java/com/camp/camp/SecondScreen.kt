0dcc9fb8dc6fff33fa304a264a50e476e6f29b8c
Rafael Caire <rafacaire1507@gmail.com>
2024-04-29 10:59:30 -0300

    Projeto finalizado

--- camp2024/Camp2024-Android/app/src/main/java/com/camp/camp/SecondScreen.kt
+++ camp2024/Camp2024-Android/app/src/main/java/com/camp/camp/SecondScreen.kt
@@ -1,6 +1,7 @@
 package com.camp.camp
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,9 +12,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -33,6 +38,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,29 +49,42 @@ import com.camp.camp.ui.theme.PinkIoasys
 @Composable
 fun SecondScreen() {
 
-    val list = remember { mutableStateListOf("item1") }
+    val list = remember { mutableStateListOf<String>() }
+    val checkedStateList = remember { mutableStateListOf<Boolean>() }
     var newItemText by remember { mutableStateOf("") }
-    
+
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.White),
         horizontalAlignment = Alignment.CenterHorizontally,
-
-        ) {
-
+    ) {
+        item {
+            Box(
+                modifier = Modifier
+                    .padding(bottom = 24.dp)
+                    .fillMaxSize()
+                    .background(PinkIoasys)
+                    .height(63.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Lista de Tarefas",
+                    fontSize = 14.sp,
+                    color = Color.White
+                )
+            }
+        }
 
         item {
             Row(
-                modifier = Modifier
-                    .padding(bottom = 16.dp)
+                modifier = Modifier.padding(bottom = 16.dp)
             ) {
                 TextField(
                     singleLine = true,
                     value = newItemText,
                     onValueChange = { newItemText = it },
-                    modifier = Modifier
-                        .padding(end = 8.dp),
+                    modifier = Modifier.padding(end = 8.dp, bottom = 41.dp),
                     textStyle = LocalTextStyle.current.copy(color = Color.Black),
                     label = {
                         Text(
@@ -72,57 +92,82 @@ fun SecondScreen() {
                             fontSize = 12.sp,
                             text = "Nova Tarefa"
                         )
-
                     },
                     shape = RoundedCornerShape(0.dp),
                     colors = TextFieldDefaults.textFieldColors(
                         containerColor = Color.Transparent,
                         unfocusedLabelColor = PinkIoasys,
                         focusedLabelColor = PinkIoasys,
-                        cursorColor = PinkIoasys
+                        cursorColor = PinkIoasys,
                     )
                 )
 
-
-                Button(modifier = Modifier,
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = PinkIoasys
-                    ),
+                Button(
+                    modifier = Modifier,
+                    colors = ButtonDefaults.buttonColors(containerColor = PinkIoasys),
                     onClick = {
                         if (newItemText.isNotBlank()) {
                             list.add(newItemText)
+                            checkedStateList.add(false)
                             newItemText = ""
                         }
                     }
                 ) {
-                    Text("ADD")
+                    Text(
+                        text ="ADD",
+                        fontSize = 12.sp,
+                        color = Color.White)
                 }
             }
         }
+
         items(
             items = list,
             key = { it }
-
         ) { message ->
             DismissContainer(
                 onDelete = {
-                    list -= message
-
+                    val index = list.indexOf(message)
+                    list.removeAt(index)
+                    checkedStateList.removeAt(index)
                 },
             ) {
-                Text(
+                val index = list.indexOf(message)
+                val isChecked = checkedStateList[index]
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
-                        .padding(bottom = 16.dp)
+                        .background(Color.White)
                         .fillMaxWidth()
-                        .height(54.dp)
-                        .background(Color.White),
-                    text = message,
-                    color = Color.Black,
-                )
+                        .padding(vertical = 8.dp),
+                ) {
+                    Icon(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        imageVector = if (isChecked) Icons.Filled.CheckCircle else Icons.Filled.Info,
+                        contentDescription = "Icone de verificação",
+                        tint = PinkIoasys
+                    )
+
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = message,
+                        color = Color.Black,
+                    )
+
+                    Checkbox(
+                        modifier = Modifier.padding(horizontal = 46.dp),
+                        checked = isChecked,
+                        onCheckedChange = { checkedStateList[index] = it },
+                        enabled = true,
+                        colors = CheckboxDefaults.colors(PinkIoasys),
+                    )
+                }
             }
         }
     }
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -131,6 +176,7 @@ private fun DismissContainer(
     content: @Composable () -> Unit,
 ) {
     val state = rememberDismissState(
+
         confirmValueChange = { value ->
             if (value == DismissValue.DismissedToEnd) {
                 onDelete()
@@ -138,9 +184,9 @@ private fun DismissContainer(
             } else {
                 false
             }
-        }
-    )
+        })
     SwipeToDismiss(
+        modifier = Modifier,
         state = state,
         background = { BackgroundDelete() },
         dismissContent = { content() },
@@ -152,14 +198,12 @@ private fun DismissContainer(
 private fun BackgroundDelete() {
     Box(
         modifier = Modifier
-            .padding(bottom = 16.dp)
             .fillMaxSize()
             .background(Color.Red),
         contentAlignment = Alignment.CenterStart
     ) {
         Icon(
-            modifier = Modifier
-                .padding(start = 26.dp),
+            modifier = Modifier.padding(start = 26.dp),
             imageVector = Icons.Default.Delete,
             contentDescription = "Icone de Lixeira",
             tint = Color.White
